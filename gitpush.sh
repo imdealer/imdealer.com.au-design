905e98b8270b519d552b40f2a885c02a28920b6d
YS <info@imdealer.com.au>
2019-04-22 21:02:25 +1000

    20190422210225 @dealer_landing video screen size fix, @dealer_landing add current bid section bg image

--- gitpush.sh
+++ gitpush.sh
@@ -3,5 +3,5 @@ set -x
 set -e
 date=$(date '+%Y%m%d%H%M%S')
 git add .
-git commit -m "$date"
+git commit -m "$date @dealer_landing video screen size fix, @dealer_landing add current bid section bg image"
 git push
