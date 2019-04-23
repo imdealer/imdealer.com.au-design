7d089048aac4071747600ad26461a48a18a6a144
YS <info@imdealer.com.au>
2019-04-23 21:26:22 +1000

    20190423212621 @user_pending *add new page is added

--- gitpush.sh
+++ gitpush.sh
@@ -3,5 +3,5 @@ set -x
 set -e
 date=$(date '+%Y%m%d%H%M%S')
 git add .
-git commit -m "$date @dealer_landing *change main title absolute position, @user_landing *change header structure same to dealer header"
+git commit -m "$date @user_pending *add new page is added"
 git push
