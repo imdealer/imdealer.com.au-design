2a89ea228f9f3d3c3b4763033fe8211bea07b667
YS <info@imdealer.com.au>
2019-04-23 22:00:53 +1000

    20190423220053 @email_dealer_email_verification *change page is totally redesigned

--- gitpush.sh
+++ gitpush.sh
@@ -3,5 +3,5 @@ set -x
 set -e
 date=$(date '+%Y%m%d%H%M%S')
 git add .
-git commit -m "$date @user_pending *add new page is added"
+git commit -m "$date @email_dealer_email_verification *change page is totally redesigned"
 git push
