c1a172ce30f41158997bbea2378104dd6059d084
YS <info@imdealer.com.au>
2019-05-26 20:24:53 +1000

    20190526202453 @user_signup *add dealer site link on the bottom of sign up form

--- gitpush.sh
+++ gitpush.sh
@@ -3,5 +3,5 @@ set -x
 set -e
 date=$(date '+%Y%m%d%H%M%S')
 git add .
-git commit -m "$date @user_allpage *add dealer site link on header"
+git commit -m "$date @user_signup *add dealer site link on the bottom of sign up form"
 git push
