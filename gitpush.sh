3b09179f6a4c560d9bf93e9b82addbfd3906c603
YS <info@imdealer.com.au>
2019-05-31 19:57:11 +1000

    20190531195711 @user_faq *modify translate Korean to English

--- gitpush.sh
+++ gitpush.sh
@@ -3,5 +3,5 @@ set -x
 set -e
 date=$(date '+%Y%m%d%H%M%S')
 git add .
-git commit -m "$date @user_signup *add dealer site link on the bottom of sign up form"
+git commit -m "$date @user_faq *modify translate Korean to English"
 git push
