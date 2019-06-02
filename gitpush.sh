b4c710d2d55eba57db984a6eb8e2855cd7274013
YS <info@imdealer.com.au>
2019-06-02 18:26:39 +1000

    20190602182638 @dealer_faq *modify some text about dealer commission

--- gitpush.sh
+++ gitpush.sh
@@ -3,5 +3,5 @@ set -x
 set -e
 date=$(date '+%Y%m%d%H%M%S')
 git add .
-git commit -m "$date @all images changed to no watermark version, dealer_landing *modify event section"
+git commit -m "$date @dealer_faq *modify some text about dealer commission"
 git push
