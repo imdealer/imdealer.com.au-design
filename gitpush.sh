3c7e7c9b42d42a47b97ded6c6a67a20baa1ebd3d
YS <info@imdealer.com.au>
2019-06-09 13:07:54 +1000

    20190609130754 @user_landing *modify Why imdealer section changes images

--- gitpush.sh
+++ gitpush.sh
@@ -3,5 +3,5 @@ set -x
 set -e
 date=$(date '+%Y%m%d%H%M%S')
 git add .
-git commit -m "$date @dealer_landing *add what is IMDEALER section"
+git commit -m "$date @user_landing *modify Why imdealer section changes images"
 git push
