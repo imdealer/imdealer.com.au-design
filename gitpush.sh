778a8e1fb503cbd256bf8912babc153c4c93d387
YS <info@imdealer.com.au>
2019-06-09 17:40:32 +1000

    20190609174032 @user_landing *add event section on the bottom of page

--- gitpush.sh
+++ gitpush.sh
@@ -3,5 +3,5 @@ set -x
 set -e
 date=$(date '+%Y%m%d%H%M%S')
 git add .
-git commit -m "$date @user_landing *modify put a tooltip to blind bidding of main catch phrase"
+git commit -m "$date @user_landing *add event section on the bottom of page"
 git push
