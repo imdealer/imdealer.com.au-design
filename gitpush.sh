dc2663a829bd23073e65a52ab86321fc7ae187dd
YS <info@imdealer.com.au>
2019-06-09 16:06:31 +1000

    20190609160631 @user_landing *modify put a tooltip to blind bidding of main catch phrase

--- gitpush.sh
+++ gitpush.sh
@@ -3,5 +3,5 @@ set -x
 set -e
 date=$(date '+%Y%m%d%H%M%S')
 git add .
-git commit -m "$date @user_landing *modify Why imdealer section changes images"
+git commit -m "$date @user_landing *modify put a tooltip to blind bidding of main catch phrase"
 git push
