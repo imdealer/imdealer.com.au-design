fca4ab1ac5b1cb49df1f5303f507d98f0aad5226
YS <info@imdealer.com.au>
2019-06-10 11:45:00 +1000

    20190610114459 @user_landing *add what is IMDEALER section       @dealer_landing *modify logo

--- gitpush.sh
+++ gitpush.sh
@@ -3,5 +3,6 @@ set -x
 set -e
 date=$(date '+%Y%m%d%H%M%S')
 git add .
-git commit -m "$date @user_landing *add carousel on header and add opening event slide"
+git commit -m "$date @user_landing *add what is IMDEALER section 
+      @dealer_landing *modify logo" 
 git push
