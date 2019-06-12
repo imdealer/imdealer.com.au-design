83f7b7c72d2834d990075cd9e1bea34619e235bf
YS <info@imdealer.com.au>
2019-06-12 21:17:25 +1000

    20190612211725 @dealer_showroom *modify showroom design modified

--- gitpush.sh
+++ gitpush.sh
@@ -3,7 +3,5 @@ set -x
 set -e
 date=$(date '+%Y%m%d%H%M%S')
 git add .
-git commit -m "$date @user_landing *modify change the content of what is IMDEALER section 
-      @dealer_landing *modify change the content of what is IMDEALER section
-      @dealer_landing *modify change the interval of caroulsel from 5000ms to 4000ms"
+git commit -m "$date @dealer_showroom *modify showroom design modified"
 git push
