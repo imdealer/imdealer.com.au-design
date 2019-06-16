8dd25aec8889a1d2cb78fa8a93b7e5b794b5781f
YS <info@imdealer.com.au>
2019-06-16 18:42:10 +1000

    20190616184210 @dealer_landing *modify all english expression(including modal) are modified         *modify minor design modified         *modify put real links on social icons of footer, remove instagram icon on footer

--- gitpush.sh
+++ gitpush.sh
@@ -3,5 +3,7 @@ set -x
 set -e
 date=$(date '+%Y%m%d%H%M%S')
 git add .
-git commit -m "$date @dealer_showroom *modify showroom design modified"
+git commit -m "$date @dealer_landing *modify all english expression(including modal) are modified
+        *modify minor design modified
+        *modify put real links on social icons of footer, remove instagram icon on footer"
 git push
