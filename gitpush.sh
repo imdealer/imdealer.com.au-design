186df3bbbcba667aadc75d22beeb9f5b3571a082
YS <info@imdealer.com.au>
2019-06-16 17:25:40 +1000

    20190616172539 @dealer_showroom *modify showroom design modified

--- gitpush.sh
+++ gitpush.sh
@@ -3,5 +3,5 @@ set -x
 set -e
 date=$(date '+%Y%m%d%H%M%S')
 git add .
-git commit -m "$date @dealer_showroom *modify showroom design modified(added view button)"
+git commit -m "$date @dealer_showroom *modify showroom design modified"
 git push
