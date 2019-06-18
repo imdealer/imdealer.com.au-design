42a31cd082eb980b0cd67dce01959e1a4f0dc3b3
YS <info@imdealer.com.au>
2019-06-18 22:25:40 +1000

    20190618222540 @user_sellmycar_step2_details *add addtional dates input form and information modal

--- gitpush.sh
+++ gitpush.sh
@@ -3,7 +3,5 @@ set -x
 set -e
 date=$(date '+%Y%m%d%H%M%S')
 git add .
-git commit -m "$date @dealer_landing *modify all english expression(including modal) are modified
-        *modify minor design modified
-        *modify put real links on social icons of footer, remove instagram icon on footer"
+git commit -m "$date @user_sellmycar_step2_details *add addtional dates input form and information modal"
 git push
