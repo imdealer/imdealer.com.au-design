2f25b09ee763792dae816e3120dc6d9e09ea8001
YS <info@imdealer.com.au>
2019-06-19 23:48:18 +1000

    20190619234818 @dealer_faq *modify english expressions are corrected     @dealer_product *modify re-order the detail section, *add The condition of vehicle and PPSR report section     @user_landing *modify english expressions are corrected in what is imdealer section     @user_sellmycar_step1_photos *modify the condition of vehicle is modified

--- gitpush.sh
+++ gitpush.sh
@@ -3,5 +3,8 @@ set -x
 set -e
 date=$(date '+%Y%m%d%H%M%S')
 git add .
-git commit -m "$date @user_sellmycar_step2_details *add addtional dates input form and information modal"
+git commit -m "$date @dealer_faq *modify english expressions are corrected
+    @dealer_product *modify re-order the detail section, *add The condition of vehicle and PPSR report section
+    @user_landing *modify english expressions are corrected in what is imdealer section
+    @user_sellmycar_step1_photos *modify the condition of vehicle is modified"
 git push
