40f46cf9e5a58b26acc5a0e2a3cb689120960580
YS <info@imdealer.com.au>
2019-06-26 22:25:06 +1000

    20190626222505 @user_mypage *modify design is specifically modified for mobile

--- gitpush.sh
+++ gitpush.sh
@@ -3,8 +3,5 @@ set -x
 set -e
 date=$(date '+%Y%m%d%H%M%S')
 git add .
-git commit -m "$date @dealer_faq *modify english expressions are corrected
-    @dealer_product *modify re-order the detail section, *add The condition of vehicle and PPSR report section
-    @user_landing *modify english expressions are corrected in what is imdealer section
-    @user_sellmycar_step1_photos *modify the condition of vehicle is modified"
+git commit -m "$date @user_mypage *modify design is specifically modified for mobile"
 git push
